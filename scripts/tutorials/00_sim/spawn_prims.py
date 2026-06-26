260e4659523fdc995c25b8d8d6597e90e8c97639
holybro6767 <holybro6767@gmail.com>
2026-06-25 22:57:12 -0700

    2nd and 3rd tutorial

--- scripts/tutorials/00_sim/spawn_prims.py
+++ scripts/tutorials/00_sim/spawn_prims.py
@@ -31,39 +31,39 @@ simulation_app = app_launcher.app
 
 """Rest everything follows."""
 
-import isaaclab.sim as sim_utils
-from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
+import isaaclab.sim as sim_utils #To spawn and modify dif materials
+from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR #Real-Time collabation platform
 
 
 def design_scene():
     """Designs the scene by spawning ground plane, light, objects and meshes from usd files."""
     # Ground-plane
-    cfg_ground = sim_utils.GroundPlaneCfg()
-    cfg_ground.func("/World/defaultGroundPlane", cfg_ground)
+    cfg_ground = sim_utils.GroundPlaneCfg() #Blueprint
+    cfg_ground.func("/World/defaultGroundPlane", cfg_ground) #Calling this func method on a cfg, it internally calls the spawn function to create prim
 
     # spawn distant light
     cfg_light_distant = sim_utils.DistantLightCfg(
         intensity=3000.0,
         color=(0.75, 0.75, 0.75),
     )
-    cfg_light_distant.func("/World/lightDistant", cfg_light_distant, translation=(1, 0, 10))
+    cfg_light_distant.func("/World/lightDistant", cfg_light_distant, translation=(1, 0, 10)) #light_distanct spawned w/ translation
 
     # create a new xform prim for all objects to be spawned under
-    sim_utils.create_prim("/World/Objects", "Xform")
+    sim_utils.create_prim("/World/Objects", "Xform") #Group
     # spawn a red cone
     cfg_cone = sim_utils.ConeCfg(
         radius=0.15,
         height=0.5,
         visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(1.0, 0.0, 0.0)),
-    )
-    cfg_cone.func("/World/Objects/Cone1", cfg_cone, translation=(-1.0, 1.0, 1.0))
-    cfg_cone.func("/World/Objects/Cone2", cfg_cone, translation=(-1.0, -1.0, 1.0))
+    ) #cfg for cone
+    cfg_cone.func("/World/Objects/Cone1", cfg_cone, translation=(-1.0, 1.0, 1.0)) #Left cone
+    cfg_cone.func("/World/Objects/Cone2", cfg_cone, translation=(-1.0, -1.0, 1.0)) #Right cone
 
-    # spawn a green cone with colliders and rigid body
+    # spawn a green cone with colliders and rigid body: cfg of cone w/ Physics added
     cfg_cone_rigid = sim_utils.ConeCfg(
         radius=0.15,
         height=0.5,
-        rigid_props=sim_utils.RigidBodyPropertiesCfg(),
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(), #Rigid body
         mass_props=sim_utils.MassPropertiesCfg(mass=1.0),
         collision_props=sim_utils.CollisionPropertiesCfg(),
         visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.0, 1.0, 0.0)),
@@ -72,20 +72,20 @@ def design_scene():
         "/World/Objects/ConeRigid", cfg_cone_rigid, translation=(-0.2, 0.0, 2.0), orientation=(0.5, 0.0, 0.5, 0.0)
     )
 
-    # spawn a blue cuboid with deformable body
+    # spawn a blue cuboid with deformable body: cfg of cube w/ deformable properties
     cfg_cuboid_deformable = sim_utils.MeshCuboidCfg(
         size=(0.2, 0.5, 0.2),
-        deformable_props=sim_utils.DeformableBodyPropertiesCfg(),
+        deformable_props=sim_utils.DeformableBodyPropertiesCfg(), #Deform, internal
         visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.0, 0.0, 1.0)),
-        physics_material=sim_utils.DeformableBodyMaterialCfg(),
+        physics_material=sim_utils.DeformableBodyMaterialCfg(), #External interactions
     )
     cfg_cuboid_deformable.func("/World/Objects/CuboidDeformable", cfg_cuboid_deformable, translation=(0.15, 0.0, 2.0))
 
-    # spawn a usd file of a table into the scene
+    # spawn a usd file of a table into the scene: Spawning prims from another file
     cfg = sim_utils.UsdFileCfg(usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/Mounts/SeattleLabTable/table_instanceable.usd")
     cfg.func("/World/Objects/Table", cfg, translation=(0.0, 0.0, 1.05))
 
-
+#All prims must be spawned before simulation starts
 def main():
     """Main function.""" #Control and manage simulation, Tutorial 1
 
